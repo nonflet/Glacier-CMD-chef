3ccc09a47cb5fe535d73bd0192fb09f56e381595
Eric Bollens <eric@eb.io>
2014-11-09 10:53:10 -0800

    Add metadata.rb file

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name 'glacier-cmd'
+version '1.0.0'
+
+maintainer 'Dmitry Lavrinenenko'
+maintainer_email 'nonflet@gmail.com'
+
+description 'Automatted backup to Glacier using github.com/uskudnik/amazon-glacier-cmd-interface'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+
+suggests 'python'
